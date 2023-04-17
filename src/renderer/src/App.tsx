2c42968ff1cef230d48db12188fad16619b4e61d
phy-lei <765373325@qq.com>
2023-04-18 01:06:49 +0800

    feat: init project

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+import Versions from './components/Versions';
+
+function App(): JSX.Element {
+  return (
+    <div className="container">
+      <Versions></Versions>
+    </div>
+  );
+}
+
+export default App;
